60871894bd8e7f482d8ac741e0c06ed4eae12668
jAcquire Kingdom <jacquire.kingdom@gmail.com>
2026-09-03 09:40:23 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'; 
+import react from "@vitejs/plugin-react"; 
 
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills({
-      protocolImports: true,
-    }), 
   ],
   server: {
     host: process.env.HOST || '0.0.0.0',
